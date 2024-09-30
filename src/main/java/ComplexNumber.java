6e9d241cc441771d59a4394d11f0efa472a58bd9
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2024-09-30 14:21:01 +0300

    first commit

---
+++ src/main/java/ComplexNumber.java
@@ -0,0 +1,62 @@
+public class ComplexNumber {
+
+    private int real;
+    private int imaginary;
+
+    public ComplexNumber() {
+        this(0, 0);
+    }
+
+    public ComplexNumber(int real) {
+        this(real, 0);
+    }
+
+    public ComplexNumber(int real, int imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public int getReal() {
+        return real;
+    }
+
+    public void setReal(int real) {
+        this.real = real;
+    }
+
+    public int getImaginary() {
+        return imaginary;
+    }
+
+    public void setImaginary(int imaginary) {
+        this.imaginary = imaginary;
+    }
+
+    public ComplexNumber add(ComplexNumber other) {
+        int resultingReal, resultingImaginary;
+        if (other == null) {
+            resultingReal = 0;
+            resultingImaginary = 0;
+        } else {
+            resultingReal = getReal() + other.getReal();
+            resultingImaginary = getImaginary() + other.getImaginary();
+        }
+        return new ComplexNumber(resultingReal, resultingImaginary);
+    }
+
+    public ComplexNumber multiply(ComplexNumber other) {
+        int resultingReal, resultingImaginary;
+        if (other == null) {
+            resultingReal = 0;
+            resultingImaginary = 0;
+        } else {
+            resultingReal = getReal() * other.getReal() - getImaginary() * other.getImaginary();
+            resultingImaginary = getReal() * other.getImaginary() + getImaginary() * other.getReal();
+        }
+        return new ComplexNumber(resultingReal, resultingImaginary);
+    }
+
+    public ComplexNumber subtract(ComplexNumber other) {
+        return add(other.multiply(new ComplexNumber(-1)));
+    }
+}
